b480bdc24275685a4e0f0c34131c7693a050c0ce
Ashrof Salman <salmanashraf853@gmil.com>
2021-12-24 23:08:22 +0100

    adding initiate trsf endpoint

--- routes/transfers.js
+++ routes/transfers.js
@@ -38,6 +38,47 @@ router.get('/banks', async res=>{
 })
 
 // generate recipient
+router.post('/create_recipient', async (req, res)=>{
+
+    const { account_number, name, bank_code } = req.body
+    const fetch_res = await fetch(`https://api.paystack.co/transferrecipient`,{
+        method: 'GET',
+        headers: {
+            Authorization: 'Bearer sk_live_97d1bdd1216103cffbe8f2582fc1a6159c558af8'
+        },
+        body:{
+            name,
+            account_number,
+            bank_code,
+            currency: 'NGN',
+            type: 'nuban'
+        }
+    })
+
+    const json = await fetch_res.json()
+    res.json(json)
+
+})
+
+
+// initiate transfer
+router.post('/init_transfer', async (req, res)=>{
+
+    const { recipient_code } = req.body
+    const fetch_res = await fetch(`https://api.paystack.co/transfer`,{
+        method: 'GET',
+        headers: {
+            Authorization: 'Bearer sk_live_97d1bdd1216103cffbe8f2582fc1a6159c558af8'
+        },
+        body:{
+            recipient_code
+        }
+    })
+
+    const json = await fetch_res.json()
+    res.json(json)
+
+})
 
 
 module.exports = router
